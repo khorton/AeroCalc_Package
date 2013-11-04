c0a4e08c42f0f6c337ccaff1dbaea8621a5cb9ae
Kevin Horton <kevin01@kilohotel.com>
2013-11-04 18:33:59 -0500

    Update node name for new sage machine

--- aerocalc/personal/prop_map.py
+++ aerocalc/personal/prop_map.py
@@ -77,6 +77,8 @@ class Prop:
                     base_path = '/home/kwh/RV/Hartzell_prop_maps/'
                 elif node_name == 'sage':
                     base_path = '/mnt/hgfs/shared/prop_maps/Hartzell_prop_maps/'
+                elif node_name == 'sage-ubuntu-1204':
+                    base_path = '/mnt/hgfs/shared/prop_maps/Hartzell_prop_maps/'
                 else:
                     raise ValueError, 'Unknown computer'
 
@@ -339,6 +341,8 @@ class Prop:
                 elif node_name == 'eeepc':
                     base_path = '/home/kwh/RV/MT_Prop/'
                 elif node_name == 'sage':
+                    base_path = '/mnt/hgfs/shared/prop_maps/'
+                elif node_name == 'sage-ubuntu-1204':
                     base_path = '/mnt/hgfs/shared/prop_maps/'
                 else:
                     raise ValueError, 'Unknown computer'
